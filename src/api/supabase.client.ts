98fe216ab6267400335414f0e8214f0d1650b3fc
ihugo1 <huglainez@gmail.com>
2025-11-26 17:09:53 -0600

    Quitar console logs

--- src/api/supabase.client.ts
+++ src/api/supabase.client.ts
@@ -3,8 +3,4 @@ import { createClient } from "@supabase/supabase-js";
 const supbaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 
-
-console.log(supabaseUrl);
-console.log(supbaseAnonKey);
-
 export const supabase = createClient(supabaseUrl, supbaseAnonKey);
